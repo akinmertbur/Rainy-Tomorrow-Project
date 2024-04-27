8d521139ed68a9870e0539ece477be0d3d29269a
Akin Mert Bur <akinmertbur@gmail.com>
2024-04-27 11:57:22 +0300

    add form and styling to index.ejs

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
+dotenv.config();
 
 // Create an express app and set the port number.
 const app = express();
 const port = 3000;
 const API_URL = "api.openweathermap.org";
+const apiKey = process.env.API_KEY;
 
 // Use the public folder for static files.
 app.use(express.static("public"));
@@ -14,6 +17,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // When the user goes to the home page it should render the index.ejs file.
 app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+// Use Axios to retrieve the weather forecast and pass it to index.ejs to
+// display whether tomorrow will be rainy or not.
+app.post("/", (req, res) => {
   res.render("index.ejs");
 });
 
