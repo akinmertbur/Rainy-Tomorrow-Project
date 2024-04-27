9815b25e7a9c141ec4082c3ada7e42a65948b805
Akin Mert Bur <akinmertbur@gmail.com>
2024-04-27 13:41:16 +0300

    Implement weather API functionality to determine tomorrow's forecast, indicating whether it will be rainy or not, utilizing latitude and longitude parameters for location.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config();
 // Create an express app and set the port number.
 const app = express();
 const port = 3000;
-const API_URL = "api.openweathermap.org";
+const API_URL = "https://api.openweathermap.org/data/2.5/forecast";
 const apiKey = process.env.API_KEY;
 
 // Use the public folder for static files.
@@ -22,9 +22,30 @@ app.get("/", (req, res) => {
 
 // Use Axios to retrieve the weather forecast and pass it to index.ejs to
 // display whether tomorrow will be rainy or not.
-app.post("/", (req, res) => {
-  const location = req.body["location"];
-  res.render("index.ejs");
+app.post("/", async (req, res) => {
+  //const location = req.body["location"];
+
+  const data = {
+    lat: 22.21232,
+    lon: -55.107964,
+    // lat: 45.21232,
+    // lon: 22.107964,
+  };
+
+  try {
+    const response = await axios.get(API_URL, {
+      params: { lat: data.lat, lon: data.lon, appid: apiKey },
+    });
+    res.render("index.ejs", {
+      morning_data: response.data.list[7].weather[0].main,
+      afternoon_data: response.data.list[9].weather[0].main,
+    });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
 });
 
 // Listen on the predefined port and start the server.
